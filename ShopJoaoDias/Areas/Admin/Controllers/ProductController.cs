6905f4461d280fb2809cecbfee0e662dc5e9ff51
JoaoDias <joaodiasworking@gmail.com>
2022-10-16 14:50:11 -0300

    Finalizando metodo de editar produto, implementando para poder ser feito upload de mutliplas imagens

--- ShopJoaoDias/Areas/Admin/Controllers/ProductController.cs
+++ ShopJoaoDias/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Interfaces.BL;
+﻿using Entities;
+using Interfaces.BL;
 using Microsoft.AspNetCore.Mvc;
 using ShopJoaoDias.Areas.Admin.Models;
 using ShopJoaoDias.Extensions;
@@ -39,7 +40,6 @@ namespace ShopJoaoDias.Areas.Admin.Controllers
             return View(model);
         }
 
-        [HttpGet]
         public IActionResult Create()
         {
             var categories = _categoryBL.GetList().Where(w => w.Parentid == 0).ToList();
@@ -128,6 +128,133 @@ namespace ShopJoaoDias.Areas.Admin.Controllers
             }
         }
 
+        public IActionResult Edit(int id)
+        {
+            var productDO = _productBL.GetById(id);
+            var categories = _categoryBL.GetList().Where(w => w.Parentid == 0).ToList();
+            var brands = _brandBL.GetList();
+            var units = _unitBL.GetList();
+
+            var model = new ProductViewModel
+            {
+                ProductDO = productDO,
+                CategoryList = categories,
+                BrandList = brands,
+                UnitList = units
+            };
+
+            return View(model);
+        }
+
+        [HttpPost, AutoValidateAntiforgeryToken]
+        public IActionResult Edit(int id, ProductViewModel model)
+        {
+            try
+            {
+                var productDO = model.ProductDO;
+                if (ModelState.IsValid)
+                {
+                    if (!string.IsNullOrEmpty(productDO.Easyurl))
+                    {
+                        var slug = Functions.FriendlyUrl(productDO.Easyurl);
+                        productDO.Easyurl = slug;
+                    }
+                    else
+                    {
+                        var slug = Functions.FriendlyUrl(productDO.Name);
+                        productDO.Easyurl = slug;
+                    }
+
+                    if (productDO.Description == null)
+                    {
+                        var desc = "All products with best price from " + productDO.Name;
+                        productDO.Description = desc;
+                    }
+
+                    if (productDO.Pagetitle == null)
+                    {
+                        productDO.Pagetitle = productDO.Name + "page title for";
+                    }
+
+                    if (productDO.Metadescription == null)
+                    {
+                        productDO.Metadescription = productDO.Name + "page meta description for";
+                    }
+
+                    var updateProduct = _productBL.Update(productDO);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    var errors = ModelState.Values.Where(x => x.Errors.Count > 0)
+                        .SelectMany(x => x.Errors)
+                        .Select(x => x.ErrorMessage)
+                        .ToList();
+
+                    foreach (var item in errors)
+                    {
+                        ViewBag.error += "* " + item.ToString() + "<br>";
+                    }
+                    var categories = _categoryBL.GetList().Where(w => w.Parentid == 0).ToList();
+                    var brands = _brandBL.GetList();
+                    var units = _unitBL.GetList();
+                    model.CategoryList = categories;
+                    model.BrandList = brands;
+                    model.UnitList = units;
+                    return View(model);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                var categories = _categoryBL.GetList().Where(w => w.Parentid == 0).ToList();
+                var brands = _brandBL.GetList();
+                var units = _unitBL.GetList();
+                model.CategoryList = categories;
+                model.BrandList = brands;
+                model.UnitList = units;
+                ViewBag.error += ex.Message;
+                return View(model);
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var productDO = _productBL.GetById(id);
+                return View(productDO);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost, AutoValidateAntiforgeryToken]
+        public IActionResult Delete(int id, BankDO productDO)
+        {
+            var product = _productBL.GetById(id);
+            try
+            {
+                bool value = _productBL.Delete(product);
+                if (value)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Something went wrong please try it again!";
+                    return View(product);
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.error = "Something went wrong please try it again!";
+                return View(product);
+            }
+        }
+
         public IActionResult SubCategories(int id)
         {
             var categoryList = _categoryBL.GetList(w => w.Parentid == id).ToList();
@@ -141,6 +268,47 @@ namespace ShopJoaoDias.Areas.Admin.Controllers
                 var uploadHelpers = new UploadHelper(_hosting);
                 var fileName = await uploadHelpers.Upload(file, "products/0");
                 return Ok("/upload/products/0" + fileName);
+            }
+            else
+            {
+                return Ok();
+            }
+        }
+
+        public IActionResult Image(int id)
+        {
+            var productDO = _productBL.GetById(id);
+            if (productDO != null)
+            {
+                var productImageList = _productImageBL.GetList(x => x.Productid == id);
+                var model = new ProductViewModel
+                {
+                    ProductDO = productDO,
+                    ProductImageList = productImageList
+                };
+
+                return View(model);
+            }
+            else
+            {
+                return BadRequest("Something went wrong!");
+            }
+        }
+
+        public async Task<IActionResult> MultipleUpload(int id, IFormFile file)
+        {
+            if (file.Length > 0)
+            {
+                var uploadHelpers = new UploadHelper(_hosting);
+                var fileName = await uploadHelpers.Upload(file, "products/" + id);
+                var productImage = new ProductImageDO
+                {
+                    Address = "/upload/products" + id + "/" + fileName,
+                    Productid = id
+                };
+
+                var result = _productImageBL.Add(productImage);
+                return Json(result);
             }
             else
             {
